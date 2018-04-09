c8aa0ef9eed1a382b448c41e54763cc47f9221e1
Justtoconfirm <office@justtoconfirm.co.uk>
2018-04-09 20:52:10 +0100

    Fix merge conflicts

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-<<<<<<< HEAD
 import { Provider } from 'react-redux'
-
-import FormField from './components/2-molecules/form-field/FormField'
-
-ReactDOM.render (
-	<Provider>
-    	<FormField labelFor='firstname' labelValue='First name' name='firstname' />
-    </Provider>,
-=======
 import App from './components/App'
 
 ReactDOM.render (
-    <App />,
->>>>>>> master
+	<Provider>
+    	<App />
+    </Provider>,
     document.getElementById('root')
 )
